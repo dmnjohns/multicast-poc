eeda0819de2b7605c1ae0d876f5d9c04026136bd
Adrian George <adrian.george@rackspace.com>
2017-10-30 09:41:11 -0500

    Created project basics using the springs boot starter

---
+++ src/main/java/org/openrepose/poc/multicast/OpenshiftMulticastApplication.java
@@ -0,0 +1,12 @@
+package org.openrepose.poc.multicast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftMulticastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftMulticastApplication.class, args);
+    }
+}
